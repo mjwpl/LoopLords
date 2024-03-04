b3d4de1a022efb766b8dab88dfe924ea321f3d30
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-04 15:39:13 +0100

    Model init

---
+++ Data/Models/Item.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Data.Models
+{
+    public class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public int LoopInDays { get; set; }
+        [Ignore]
+        private int? _daysBeforeWarning {  get; set; }
+        public int? DaysBeforeWarning
+        {
+            get => _daysBeforeWarning;
+            set
+            {
+                if (LoopInDays == 1)
+                {
+                    // TODO: Localization will be required here
+                    throw new InvalidOperationException("LoopInDays cannot be 1 when setting DaysBeforeWarning.");
+                }
+
+                if (value.HasValue && value < LoopInDays)
+                {
+                    _daysBeforeWarning = value;
+                }
+                else
+                {
+                    // TODO: Localization will be required here
+                    throw new ArgumentOutOfRangeException(nameof(value), "Value must be less than LoopInDays.");
+                }
+            }
+        }
+        [OneToMany(CascadeOperations = CascadeOperation.CascadeDelete)]
+        public IEnumerable<ItemHistory> History { get; set; } = Enumerable.Empty<ItemHistory>();
+        [Ignore]
+        public bool Warning
+        {
+            get
+            {
+                if (DaysBeforeWarning.HasValue && History.Any())
+                {
+                    var lastHistoryDate = History.Max(h => h.Done);
+                    var daysSinceLastHistory = (DateTime.Now - lastHistoryDate).Days;
+                    return daysSinceLastHistory < LoopInDays - DaysBeforeWarning;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Data/Models/ItemHistory.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Data.Models
+{
+    public class ItemHistory
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Done { get; set; }
+        [ForeignKey(typeof(Item))]
+        public int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+    }
+}
