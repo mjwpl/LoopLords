02833f29232622204467b561c4b24e48449b5e64
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-05 15:46:54 +0100

    Add documentation

--- Data/Models/ItemHistory.cs
+++ Data/Models/ItemHistory.cs
@@ -3,13 +3,28 @@ using SQLiteNetExtensions.Attributes;
 
 namespace Data.Models
 {
+    /// <summary>
+    /// Represents the history of occurrences for an item.
+    /// </summary>
     public class ItemHistory
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        /// <summary>
+        /// The date and time when the occurrence was recorded.
+        /// </summary>
         public DateTime Done { get; set; }
+
+        /// <summary>
+        /// The ID of the item associated with this history record.
+        /// </summary>
         [ForeignKey(typeof(Item))]
         public int ItemId { get; set; }
+
+        /// <summary>
+        /// The item associated with this history record.
+        /// </summary>
         public virtual Item Item { get; set; }
     }
 }
