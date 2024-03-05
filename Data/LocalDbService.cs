73bfd6b4e4587d64746f2b36338d742f746d345f
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-05 14:06:00 +0100

    Database service init

---
+++ Data/LocalDbService.cs
@@ -0,0 +1,24 @@
+﻿using Data.Models;
+using SQLite;
+using SQLiteNetExtensionsAsync.Extensions;
+
+namespace Data
+{
+    public class LocalDbService
+    {
+        private SQLiteAsyncConnection db;
+
+        public LocalDbService(SQLiteAsyncConnection _connection)
+        {
+            //db = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DbName));
+            db = _connection ?? throw new ArgumentNullException(nameof(_connection));
+            db.ExecuteAsync("PRAGMA foreign_keys = ON;");
+        }
+
+        public async void Init()
+        {
+            await db.CreateTableAsync<Item>();
+            await db.CreateTableAsync<ItemHistory>();
+        }
+    }
+}
