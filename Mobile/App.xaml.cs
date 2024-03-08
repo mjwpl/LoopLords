8b9e6c5de44870690e6b41b76e28212de3f8fb74
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-08 19:15:24 +0100

    MAUI Project init

---
+++ Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
