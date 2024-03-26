279552211e114bab5549270f15e81503dcbb3511
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-26 17:51:42 +0100

    First run

--- Mobile/App.xaml.cs
+++ Mobile/App.xaml.cs
@@ -1,12 +1,54 @@
-﻿namespace Mobile
+﻿using Mobile.Data;
+using Mobile.Data.Enums;
+using Mobile.Data.Helpers;
+using Mobile.Data.Models;
+using Mobile.View;
+
+namespace Mobile
 {
     public partial class App : Application
     {
-        public App()
+        LocalDbContext _db;
+        public App(LocalDbContext db)
         {
             InitializeComponent();
+            _db = db;
 
-            MainPage = new AppShell();
+            MainPage = (FirstRun()) ? new IntroView() : new AppShell();
+         
+        }
+
+        private bool FirstRun()
+        {
+            var ft = _db.Settings.FirstOrDefault(x => x.Key == SettingsKeyEnum.RUN_COUNTER);
+
+            if (ft != null)
+            {
+                if (ft.Value == "0")
+                {
+                    ft.Value = "1";
+                    _db.SaveChanges();
+
+                    return true;
+                }
+                else
+                {
+                    ft.Value = Value.Increment(ft.Value);
+
+                    var lastDate = _db.Settings.FirstOrDefault(x => x.Key == SettingsKeyEnum.RUN_LAST_DATE);
+                    if (lastDate != null)
+                    {
+                        lastDate.Value = DateTime.Now.ToString();
+                        _db.SaveChanges();
+
+                        
+                    }
+
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
