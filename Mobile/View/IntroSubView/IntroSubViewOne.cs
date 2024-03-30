829f3ac1885826e2a368a1e790d89152acae8bad
Mariusz Jakubowski <mariusz@mjw.pl>
2024-03-30 17:55:37 +0100

    Intro Sybpage

---
+++ Mobile/View/IntroSubView/IntroSubViewOne.cs
@@ -0,0 +1,33 @@
+namespace Mobile.View.IntroSubView;
+
+public class IntroSubViewOne : ContentView
+{
+	public IntroSubViewOne()
+	{
+        var stack = new StackLayout { Orientation = StackOrientation.Vertical};
+        //stack.Children.Add(new Image
+        //    {
+        //        Source = "bg1.jpg",
+        //        Aspect = Aspect.AspectFill
+        //    }
+        //);
+
+        var btn = new Button
+        {
+            Text = "Start",
+        };
+
+        btn.Clicked += MyButton_Clicked;
+
+        stack.Children.Add(btn);
+       
+
+        void MyButton_Clicked(object sender, EventArgs e)
+        {
+            if (Application.Current != null) Application.Current.MainPage = new AppShell();
+        }
+
+        Content = stack;
+    }
+
+}
