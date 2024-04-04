b681ab200ae94702bd95edef44a81438e7fc620a
Mariusz Jakubowski <mariusz@mjw.pl>
2024-04-04 20:42:03 +0200

    Add DI helper

---
+++ Mobile/Helpers/ServiceHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Mobile.Helpers
+{
+    public static class ServiceHelper
+    {
+        public static TService GetService<TService>()
+            => Current.GetService<TService>();
+
+        public static IServiceProvider Current =>
+#if ANDROID
+            MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+            IPlatformApplication.Current.Services;
+#else
+            null;
+#endif
+    }
+
+}
