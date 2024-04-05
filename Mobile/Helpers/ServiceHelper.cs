b9c16f2cd9a1b8aa1b2968ad6992bf3d56704654
Mariusz Jakubowski <mariusz@mjw.pl>
2024-04-05 20:04:32 +0200

    Add win

--- Mobile/Helpers/ServiceHelper.cs
+++ Mobile/Helpers/ServiceHelper.cs
@@ -3,13 +3,17 @@
     public static class ServiceHelper
     {
         public static TService GetService<TService>()
+#pragma warning disable CS8603
             => Current.GetService<TService>();
+#pragma warning restore CS8603
 
         public static IServiceProvider Current =>
 #if ANDROID
             MauiApplication.Current.Services;
 #elif IOS || MACCATALYST
-            IPlatformApplication.Current.Services;
+            MauiUIApplicationDelegate.Current.Services;
+#elif WINDOWS10_0_17763_0_OR_GREATER
+            MauiWinUIApplication.Current.Services;
 #else
             null;
 #endif
