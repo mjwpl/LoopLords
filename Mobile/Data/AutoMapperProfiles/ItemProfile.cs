68a575ec852e1c253840335b99183900e36222d6
Mariusz Jakubowski <mariusz@mjw.pl>
2024-04-16 20:53:17 +0200

    Add Automapper

---
+++ Mobile/Data/AutoMapperProfiles/ItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mobile.Data.Models;
+
+namespace Mobile.Data.AutoMapperProfiles
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<Item, ItemDto>()
+                .ForMember(dest => dest.IsCheckedToday, opt => opt.MapFrom(src => src.IsCheckedToday()))
+                .ForMember(dest => dest.NextScheduledDate, opt => opt.MapFrom(src => src.GetNextScheduledDateString()));
+        }
+    }
+}
---
+++ Mobile/Data/Models/ItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mobile.Data.Models
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public int LoopInDays { get; set; }
+        public int? DaysSinceLastOccurrence { get; set; }
+        public bool IsCheckedToday { get; set; } = false;
+        public string NextScheduledDate { get; set; } = String.Empty;
+    }
+}
