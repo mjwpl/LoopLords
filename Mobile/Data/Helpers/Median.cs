6ffc70b76114079d1b957cd75ff116036a32c230
Mariusz Jakubowski <mariusz@mjw.pl>
2024-05-05 17:02:20 +0200

    Add Mediana method

---
+++ Mobile/Data/Helpers/Median.cs
@@ -0,0 +1,17 @@
+﻿public static class EnumerableExtensions
+{
+    public static double Median(this IEnumerable<double> source)
+    {
+        var sortedList = source.OrderBy(x => x).ToList();
+        int count = sortedList.Count;
+        if (count % 2 == 0)
+        {
+            int midIndex = count / 2;
+            return (sortedList[midIndex - 1] + sortedList[midIndex]) / 2;
+        }
+        else
+        {
+            return sortedList[count / 2];
+        }
+    }
+}
