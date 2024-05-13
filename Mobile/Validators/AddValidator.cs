77d2a6185761b0771f1ddc2421b11486baa8fe21
Mariusz Jakubowski <mariusz@mjw.pl>
2024-05-13 21:45:36 +0200

    Fluent Validation

---
+++ Mobile/Validators/AddValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Mobile.Data.Models;
+
+namespace Mobile.Validators
+{
+    public class AddValidator : AbstractValidator<Item>    {
+
+        public AddValidator()
+        {
+            RuleFor(vm => vm.Name).NotEmpty().WithMessage("Name is required");
+        }
+    }
+}
