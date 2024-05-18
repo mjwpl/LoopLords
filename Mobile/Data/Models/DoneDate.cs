01372e567ea35650b6e2b4c26fd314916d1075b0
Mariusz Jakubowski <mariusz@mjw.pl>
2024-05-18 19:44:08 +0200

    Model display date

---
+++ Mobile/Data/Models/DoneDate.cs
@@ -0,0 +1,18 @@
+﻿namespace Mobile.Data.Models
+{
+    public class DoneDate
+    {
+        public string DisplayDate {
+            get
+            {
+                if (RealDate.Date == DateTime.Now.Date)
+                    return "today";
+                else if (RealDate.Date == DateTime.Now.AddDays(-1).Date)
+                    return "yesterday";
+                else
+                    return RealDate.ToString("dd-MM-yyyy"); // TODO: use a format string for country
+            }
+        }
+        public DateTime RealDate { get; set; }
+    }
+}
