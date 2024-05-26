fa36a9be9aaa692af3890bee9958354434e43b9b
Mariusz Jakubowski <mariusz@mjw.pl>
2024-05-26 15:45:27 +0200

    List of welldone

---
+++ Mobile/Pages/HistoryPage.xaml.cs
@@ -0,0 +1,50 @@
+using Mobile.Data.Models;
+using Mobile.Localize;
+using Mobile.ViewModel;
+using System.ComponentModel;
+
+namespace Mobile.Pages;
+
+public partial class HistoryPage : ContentPage
+{
+    public HistoryViewModel _hvm;
+    public Item Item;
+
+    public HistoryPage(HistoryViewModel hvm)
+    {
+        InitializeComponent();
+        _hvm = hvm;
+        BindingContext = _hvm;
+        _hvm.PropertyChanged += OnPropertyChanged;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _hvm.LoadItemHistory(Item);
+    }
+
+    private async void Delete_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is HistoryViewModel itemHistory)
+        {
+            Button button = (Button)sender;
+            int itemId = (int)button.CommandParameter;
+
+            bool answer = await DisplayAlert(AppRes.POPUP_CONFIRMATION_TITLE, AppRes.POPUP_CONFIRMATION_CONTENT, AppRes.YES, AppRes.CANCEL);
+
+            if (answer) _hvm.DeleteItemHistory(itemId);
+        }
+    }
+
+    private async void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        await _hvm.LoadItemHistory(Item);
+
+        if (e.PropertyName == nameof(_hvm.ItemsHistoryList))
+        {
+            colv.ItemsSource = null;
+            colv.ItemsSource = _hvm.ItemsHistoryList;
+        }
+    }
+}
---
+++ Mobile/ViewModel/HistoryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Mobile.Services;
+using System.Collections.ObjectModel;
+using Mobile.Data.Models;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace Mobile.ViewModel
+{
+    public partial class HistoryViewModel : ObservableObject
+    {
+        public DbService _dbService;
+        public ObservableCollection<ItemHistory> ItemsHistoryList { get; set; } = new();
+
+        [RelayCommand]
+        public async Task LoadItemHistory(Item item)
+        {
+            var loadedItemHistory = await _dbService.GetItemHistory(item);
+
+
+            ItemsHistoryList.Clear();
+            foreach (var itemHistory in loadedItemHistory)
+            {
+                ItemsHistoryList.Add(itemHistory);
+            }
+        }
+
+        public async Task DeleteItemHistory(int id)
+        {
+            var itemHistory = ItemsHistoryList.FirstOrDefault(f => f.Id == id);
+            await _dbService.RemoveItemHistory(itemHistory);
+            ItemsHistoryList.Remove(itemHistory);
+        }
+    }
+}
