307a694ba0a1383ccc783d87fb1b69dca998a5c5
Mariusz Jakubowski <mariusz@mjw.pl>
2024-05-29 20:18:22 +0200

    Push Notification

---
+++ Mobile/Helpers/LocalPush.cs
@@ -0,0 +1,21 @@
+﻿using Plugin.LocalNotification;
+
+namespace Mobile.Helpers
+{
+    public class LocalPushNotification
+    {
+        public static void Register(int hour)
+        {
+            var req = new NotificationRequest
+            {
+                NotificationId = 31337,
+                Title = "Loop Lords",
+                Description = "Czy pamiętasz o dzisiejszych zadaniach?",
+                Subtitle = "Oznacz zadania",
+                Schedule = new NotificationRequestSchedule { NotifyTime = DateTime.Today.AddHours(22) }
+            };
+
+            LocalNotificationCenter.Current.Show(req);
+        }
+    }
+}
