0db3d14e1827328c067c95cab725f30676fa0214
Mariusz Jakubowski <mariusz@mjw.pl>
2024-06-02 17:59:07 +0200

    Init

---
+++ Mobile/Helpers/MedianToDisplayConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Mobile.Helpers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public class MedianToDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return "?";
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
