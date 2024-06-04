6fdcefb39946bf60f71a0ccb9134fe20a011ef13
Mariusz Jakubowski <mariusz@mjw.pl>
2024-06-04 07:09:37 +0200

    Median moved

--- Mobile/Data/Helpers/Value.cs
+++ Mobile/Data/Helpers/Value.cs
@@ -6,5 +6,20 @@
         {
             return (Int32.Parse(value) + 1).ToString();
         }
+
+        public static double Median(this IEnumerable<double> source)
+        {
+            var sortedList = source.OrderBy(x => x).ToList();
+            int count = sortedList.Count;
+            if (count % 2 == 0)
+            {
+                int midIndex = count / 2;
+                return (sortedList[midIndex - 1] + sortedList[midIndex]) / 2;
+            }
+            else
+            {
+                return sortedList[count / 2];
+            }
+        }
     }
 }
