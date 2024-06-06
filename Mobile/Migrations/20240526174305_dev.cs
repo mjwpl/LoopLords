dee5079516fb7f429e14140c6db59715acc07807
Mariusz Jakubowski <mariusz@mjw.pl>
2024-06-06 16:19:42 +0200

    Migrations

---
+++ Mobile/Migrations/20240526174305_dev.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mobile.Migrations
+{
+    /// <inheritdoc />
+    public partial class dev : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Icon = table.Column<string>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    HideInDays = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Key = table.Column<int>(type: "INTEGER", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "History",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Done = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ItemId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_History", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_History_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Settings",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { 0, "" },
+                    { 1, "0" },
+                    { 2, "" },
+                    { 3, "" },
+                    { 4, "false" },
+                    { 5, "22" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_ItemId",
+                table: "History",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "History");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ Mobile/Migrations/20240602132709_dev-2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mobile.Migrations
+{
+    /// <inheritdoc />
+    public partial class dev2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Key",
+                keyValue: 4,
+                column: "Value",
+                value: "FALSE");
+
+            migrationBuilder.InsertData(
+                table: "Settings",
+                columns: new[] { "Key", "Value" },
+                values: new object[] { 6, "TRUE" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Settings",
+                keyColumn: "Key",
+                keyValue: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Key",
+                keyValue: 4,
+                column: "Value",
+                value: "false");
+        }
+    }
+}
