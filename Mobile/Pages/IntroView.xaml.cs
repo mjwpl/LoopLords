053df5e169248e929e39a583c926ef8bd35345a1
Mariusz Jakubowski <mariusz@mjw.pl>
2024-06-08 08:05:09 +0200

    Formatting

--- Mobile/Pages/IntroView.xaml.cs
+++ Mobile/Pages/IntroView.xaml.cs
@@ -1,18 +1,21 @@
 ﻿using Mobile.Pages.IntroSubView;
+using Mobile.ViewModel;
 
 namespace Mobile.Pages;
 
 public partial class IntroView : ContentPage
 {
-    private List<ContentView> _pages = new List<ContentView> { new IntroSubViewOne() };
+    private readonly TasksViewModel _vm;
+    private List<ContentView> _pages = new List<ContentView>();
 
-    public IntroView()
+    public IntroView(TasksViewModel vm)
     {
         InitializeComponent();
+        _vm = vm;
         NavigationPage.SetHasNavigationBar(this, false);
 
         carousel.ItemsSource = _pages;
+        _pages.Add(new IntroSubViewOne(_vm));
 
-       
     }
 }
